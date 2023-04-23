6949016c5e983b2b0514e6f2570612031a36cdf6
sikontoure <131483170+sikontoure@users.noreply.github.com>
2023-04-23 03:01:53 -0500

    Update test_account.py
    Used the first draft of code instead of the final one I written out

--- test_account.py
+++ test_account.py
@@ -6,36 +6,49 @@ class Test:
     def setup_method(self):
         self.a1 = Account('John')
 
+
     def teardown_method(self):
         del self.a1
 
     def test_init(self):
         assert self.a1.get_name() == 'John'
+        assert self.a1.get_balance() == 0
 
     def test_deposit(self):
-        assert self.a1.deposit(-1.5) is False
-        assert self.a1.get_balance() == 0
-
-        assert self.a1.deposit(0) is False
-        assert self.a1.get_balance() == 0
-
-        assert self.a1.deposit(1) is True
-        assert self.a1.get_balance() == 1
+        assert self.a1.deposit(2) is True
+        assert self.a1.get_balance() == 2
 
         assert self.a1.deposit(1.5) is True
-        assert self.a1.get_balance() == 1.5
+        assert self.a1.get_balance() == 3.5
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 3.5
+
+        assert self.a1.deposit(-1.5) is False
+        assert self.a1.get_balance() == 3.5
+
+        assert self.a1.deposit(3) is True
+        assert self.a1.get_balance() == 6.5
 
     def test_withdraw(self):
         assert self.a1.withdraw(-1.5) is False
         assert self.a1.get_balance() == 0
 
+        self.a1.deposit(75)
+
         assert self.a1.withdraw(0) is False
-        assert self.a1.get_balance() == 0
+        assert self.a1.get_balance() == 75
 
-        assert self.a1.withdraw(1) is True
-        assert self.a1.get_balance() == -1
+        assert self.a1.withdraw(100) is False
+        assert self.a1.get_balance() == 75
 
-        assert self.a1.withdraw(5) is True
-        assert self.a1.get_balance() == 5
+        assert self.a1.withdraw(10.5) is True
+        assert self.a1.get_balance() == 64.5
+
+        assert self.a1.withdraw(50) is True
+        assert self.a1.get_balance() == 14.5
+
+
+
 
 
